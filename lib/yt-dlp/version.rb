08d66630f7116876fe4c492ec9fb0dd1bd7f86c1
André Perdigão <andrepcg@gmail.com>
2022-03-06 15:27:34 +0000

    Upgrade to v0.2.0

--- lib/yt-dlp/version.rb
+++ lib/yt-dlp/version.rb
@@ -7,5 +7,5 @@
 module YtDlp
   # Semantic Version as well as the bundled binary version.
   # "(major).(minor).(teeny).(pre-release).(binary-version)"
-  VERSION = '0.1.0'
+  VERSION = '0.2.0'
 end
